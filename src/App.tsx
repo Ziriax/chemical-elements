3f6ec381ab3be93510aa1bbe6561551a17ec93a9
Peter Verswyvelen <bugfact@gmail.com>
2022-06-12 17:22:13 +0200

    Trying with .nojekyll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ballImage from "./ball.jpg";
 
-const tableBySymbol = {
+const tableBySymbol: Record<string, string> = {
   H: "waterstof",
   He: "helium",
   Li: "lithium",
@@ -122,49 +122,116 @@ const tableBySymbol = {
   // At: "astaat",
   // Rn: "radon",
   // Fr: "francium",
-} as const;
+};
 
-const tableByName = Object.entries(tableBySymbol).reduce<Record<string, string>>((table, [s,n]) => {table[n] = s;return table}, {});
+const tableByName = Object.entries(tableBySymbol).reduce<Record<string, string>>((table, [s, n]) => { table[n] = s; return table }, {});
 
 function App() {
 
-  const randomEntries = React.useMemo(() => {
-    // TODO: Sort by mistakes first.
-    const entries = Object.entries(tableBySymbol);
-    for (let i=0; i<entries.length; ++i) {
+  const [table, setTable] = React.useState(tableByName);
+
+  const storageKey = table === tableByName ? "wrong-names" : "wrong-symbols"
+
+  const autoCapitalize = table === tableByName ? "false" : "true";
+
+  const normalizeInput = React.useMemo(() => table === tableByName
+    ? (a: string) => a.trim().toLowerCase()
+    : (a: string) => a.trim(), [table]);
+
+  const onGuessNames = React.useCallback(() => {
+    setTable(tableByName);
+    setCorrect(0);
+    setWrong(0);
+    setCurrentIndex(0);
+  }, []);
+
+  const onGuessSymbols = React.useCallback(() => {
+    setTable(tableBySymbol);
+    setCorrect(0);
+    setWrong(0);
+    setCurrentIndex(0);
+  }, []);
+
+  const [reset, setReset] = React.useState(0);
+
+  const [randomEntries, previousWrongCount] = React.useMemo(() => {
+    // Remove previous wrong guesses.
+    const wrongKeys: Record<string, boolean> = JSON.parse(localStorage.getItem(storageKey) || "{}");
+    const entries = Object.entries(table).filter(([key]) => !wrongKeys[key]);
+
+    for (let i = 0; i < entries.length; ++i) {
       const j = Math.floor(Math.random() * entries.length);
       const t = entries[i];
       entries[i] = entries[j];
       entries[j] = t;
     }
-    return entries;
-  }, []);
+
+    const wrongList = Object.keys(wrongKeys);
+    // Insert wrong symbols first
+    wrongList.forEach(key => {
+      entries.unshift([key, table[key]]);
+    });
+
+    return [entries, wrongList.length + 0 * reset];
+  }, [reset, storageKey, table]);
+
+  const [correct, setCorrect] = React.useState(0);
+  const [wrong, setWrong] = React.useState(0);
 
   const [currentIndex, setCurrentIndex] = React.useState(0);
 
-  const [currentSymbol, currentName] = randomEntries[currentIndex];
+  const [currentAnswer, currentQuestion] = randomEntries[currentIndex];
 
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const correctRef = React.useRef<HTMLDivElement>(null);
+  const wrongRef = React.useRef<HTMLDivElement>(null);
+
+  const lastWrongRef = React.useRef(-1);
 
   const onKey = React.useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (inputRef.current && e.key === "Enter") {
+    if (correctRef.current && wrongRef.current && inputRef.current && e.key === "Enter") {
       const guess = e.currentTarget.value;
-      inputRef.current.value = "";
-      if (guess !== currentSymbol) {
-        alert("Fout!");
+      if (normalizeInput(guess) !== normalizeInput(currentAnswer)) {
+        if (lastWrongRef.current !== currentIndex) {
+          lastWrongRef.current = currentIndex;
+          setWrong(w => w + 1);
+        }
+
+        correctRef.current.className = "";
+        wrongRef.current.className = "wrong";
+
+        const wrongItems: Record<string, boolean> = JSON.parse(localStorage.getItem(storageKey) || "{}");
+        wrongItems[currentAnswer] = true;
+        localStorage.setItem(storageKey, JSON.stringify(wrongItems));
       } else {
+        inputRef.current.value = "";
+
+        if (lastWrongRef.current !== currentIndex) {
+          const wrongItems: Record<string, boolean> = JSON.parse(localStorage.getItem(storageKey) || "{}");
+          delete wrongItems[currentAnswer];
+          localStorage.setItem(storageKey, JSON.stringify(wrongItems));
+
+          setCorrect(c => c + 1);
+        }
+
+        correctRef.current.className = "correct";
+        wrongRef.current.className = "";
+
+        lastWrongRef.current = -1;
+
         setCurrentIndex(index => {
           const next = index + 1;
-          if (next ===randomEntries.length) {
+          if (next === randomEntries.length) {
             alert("Klaar!");
-            return index;
+            setReset(r => r + 1);
+            return 0;
           } else {
             return next;
           }
         })
       }
     }
-  }, [currentSymbol, randomEntries.length]);
+  }, [normalizeInput, currentAnswer, currentIndex, storageKey, randomEntries.length]);
 
   React.useEffect(() => {
     if (inputRef.current) {
@@ -173,9 +240,30 @@ function App() {
   }, [currentIndex]);
   return (
     <div className="App">
-      <code>{currentName}</code>
+      <div>
+        <button onClick={onGuessNames}>Namen raden</button>
+        &nbsp;
+        <button onClick={onGuessSymbols}>Symbolen raden</button>
+      </div>
+      <br />
+      <div>
+        <div ref={correctRef}>
+          <code>Juist:</code>
+          <code>{correct}/{correct + wrong}</code>
+        </div>
+        <br />
+        <div ref={wrongRef}>
+          <code>Fout:</code>
+          <code>{wrong}/{correct + wrong}</code>
+        </div>
+      </div>
+      <br />
+      <code className={currentIndex < previousWrongCount ? "retry" : ""}>{currentQuestion}</code>
       &nbsp;
-      <input ref={inputRef} onKeyUp={onKey} />
+      <input ref={inputRef} onKeyUp={onKey}  autoComplete="off" autoCorrect="off" autoCapitalize={autoCapitalize} spellCheck="false" />
+      <br />
+      <br />
+      <img src={ballImage} alt="ball" />
     </div>
   );
 }
